bdf39e132a13bf0b2d30545950419fd59de61b5f
vagrant <vagrant@vagrant.vm>
2020-09-20 21:58:04 +0000

    Fix small problem.

--- db_manager.py
+++ db_manager.py
@@ -12,6 +12,7 @@ class MYSQL_CONNECTOR:
         self.USER = config.db_user
         self.PASSWORD = config.db_password
         self.DB_NAME = config.db_name
+
     def connector(self, host, user, password, database):
         try:
             connection = mysql.connector.connect(host=host,
@@ -32,20 +33,22 @@ class MYSQL_CONNECTOR:
             cursor = self.CONNECTON.cursor()
             cursor.execute(query)
             self.CONNECTON.commit()
-            print("Query works successfully!")
         except mysql.connector.errors.DatabaseError as database_error:
             print(database_error)
             sys.exit(1)
+
 
 def main():
     connector = MYSQL_CONNECTOR()
     for query  in connector.QUERIES:
         connector.run_mysql_query(connector.QUERIES[query])
-    
+        print("{} works successfully!".format(query))
+
     if (connector.CONNECTON.is_connected()):
         connector.CONNECTON.close()
         print("MySQL connection is closed")
         sys.exit(1)
+
 
 if __name__ == '__main__':
     main()
